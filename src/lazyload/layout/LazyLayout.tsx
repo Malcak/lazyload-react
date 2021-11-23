7fb23cfb9834425275b689adf740c5dabea5ee8e
Malcak <malcakx@gmail.com>
2021-11-22 19:48:11 -0500

    refactor(lazyload): add nested navigation component

--- src/lazyload/layout/LazyLayout.tsx
+++ src/lazyload/layout/LazyLayout.tsx
@@ -1,38 +1,10 @@
-import { Router, Switch, Route } from 'wouter'
-import ActiveLink from '../../components/ActiveLink'
-import LazyPage1 from '../pages/LazyPage1'
-import LazyPage2 from '../pages/LazyPage2'
-import LazyPage3 from '../pages/LazyPage3'
+import Nav from "../router/Nav";
 
 const LazyLayout = () => {
   return (
     <div>
       <h1>LazyLayout Page</h1>
-      <Router base="/lazyload">
-        <ul>
-          <li>
-            <ActiveLink href="/lazy1">Lazy 1</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink href="/lazy2">Lazy 2</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink href="/lazy3">Lazy 3</ActiveLink>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/lazy1">
-            <LazyPage1 />
-          </Route>
-          <Route path="/lazy2">
-            <LazyPage2 />
-          </Route>
-          <Route path="/lazy3">
-            <LazyPage3 />
-          </Route>
-        </Switch>
-      </Router>
+      <Nav />
     </div>
   )
 }
---
+++ src/lazyload/router/Nav.tsx
@@ -0,0 +1,40 @@
+import { Router, Switch, Route } from 'wouter'
+import ActiveLink from '../../components/ActiveLink'
+import LazyPage1 from '../pages/LazyPage1'
+import LazyPage2 from '../pages/LazyPage2'
+import LazyPage3 from '../pages/LazyPage3'
+
+const Nav = () => {
+  return (
+    <div>
+      <Router base="/lazyload">
+        <ul>
+          <li>
+            <ActiveLink href="/lazy1">Lazy 1</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/lazy2">Lazy 2</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink href="/lazy3">Lazy 3</ActiveLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/lazy1">
+            <LazyPage1 />
+          </Route>
+          <Route path="/lazy2">
+            <LazyPage2 />
+          </Route>
+          <Route path="/lazy3">
+            <LazyPage3 />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+
+export default Nav
