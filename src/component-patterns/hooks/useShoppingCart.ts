82f2ad6494fdf1954fb66668a666bc09965d43cd
Malcak <malcakx@gmail.com>
2021-11-28 17:06:57 -0500

    refacto(control-props): add not truly control props implementation

--- src/component-patterns/hooks/useShoppingCart.ts
+++ src/component-patterns/hooks/useShoppingCart.ts
@@ -14,36 +14,19 @@ export const useShoppingCart = () => {
     product: Product
   }) => {
     setShoppingCart(prevCartState => {
-      const productInCart: ProductInCart = prevCartState[product.id] || {
-        ...product,
-        count: 0
+      // not truly control props
+
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = prevCartState
+        return rest
       }
 
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count
-        return {
-          ...prevCartState,
-          [product.id]: productInCart
-        }
+      return {
+        ...prevCartState,
+        [product.id]: { ...product, count }
       }
-
-      const { [product.id]: toDelete, ...rest } = prevCartState
-      return rest
     })
   }
 
   return { shoppingCart, onProducCountChange }
 }
-
-/* old onProducCountChange implementation without truly control props
-  if (count === 0) {
-    const { [product.id]: toDelete, ...rest } = prevCartState
-    console.log(toDelete)
-    return rest
-  }
-
-  return {
-    ...prevCartState,
-    [product.id]: { ...product, count }
-  }
-*/
